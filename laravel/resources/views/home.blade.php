1fa596630b2d903abb3744e225c34e3f659a383d
memojg <memo_is_22@hotmail.com>
2017-05-09 15:00:32 -0600

    se agrega home y cambia wecome

---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('principal2')
+
+@section('encabezado')
+<h2>Inicio</h2>
+@stop
+
+@section('contenido')
+<div class="jumbotron">
+  <h1>Sistema de Administración de Proyectos</h1>
+  <p>Sistema de administración de proyectos, permite llevar un control adecuado de tu proyecto implementado las mejores prácticas de trabajo, para obtener proyectos de calidad.</p>
+  <p><a class="btn btn-primary btn-lg">Más info.</a></p>
+</div>
+@stop
